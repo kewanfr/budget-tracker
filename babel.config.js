47f5a519428cb5852d439015f25688b54e4f3bcd
Kéwan B <mail@kewan.fr>
2024-10-26 21:14:21 +0200

    nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+  const plugins = [];
+
+  plugins.push('react-native-reanimated/plugin');
   return {
-    presets: ['babel-preset-expo'],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    plugins,
   };
 };
